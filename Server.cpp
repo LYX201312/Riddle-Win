6afc12fe2fddda7b16820f9bf96edc4aec3e5745
LYX201312 <LYX201312@outlook.com>
2025-06-13 11:09:40 +0800

    Update Server.cpp

--- Server.cpp
+++ Server.cpp
@@ -1,15 +1,15 @@
 #include<winsock2.h>
 #include<iostream>
-#define DEFPORT 5050
+#define DEFPORT 25565
 
 using namespace std;
 
-int map[32][32]={1};
+int map[32][32] = {1};
 
-int main(){
+int main() {
 	system("title Server-Beta1");
 	WSADATA wsadata;
-	WSAStartup(MAKEWORD(2,2),&wsadata);
+	WSAStartup(MAKEWORD(2, 2), &wsadata);
 	SOCKET s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (s == INVALID_SOCKET) {
 		printf("Socket creation failed\n");
@@ -20,8 +20,8 @@ int main(){
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(DEFPORT);
 	addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	
-	if (bind(s, (SOCKADDR*)&addr, sizeof(addr)) == SOCKET_ERROR) {
+
+	if (bind(s, (SOCKADDR * )&addr, sizeof(addr)) == SOCKET_ERROR) {
 		printf("Bind failed\n");
 		closesocket(s);
 		WSACleanup();
@@ -33,36 +33,37 @@ int main(){
 		WSACleanup();
 		return 1;
 	}
-	SOCKET client_socket = accept(s, NULL, NULL);
-	while(1){
-	if (client_socket == INVALID_SOCKET) {
-		printf("Accept failed\n");
-		closesocket(s);
-		WSACleanup();
-		return 1;
-	}
-	char recvbuf[512];
-	int bytes_received = recv(client_socket, recvbuf, sizeof(recvbuf), 0);
-	if (bytes_received > 0) {
-		recvbuf[bytes_received] = '\0';
-		printf("Received: %s\n", recvbuf);
-	}
-		if(recvbuf=="map"){
+	SOCKET client_socket;
+	while (1) {
+		client_socket = accept(s, NULL, NULL);
+		if (client_socket == INVALID_SOCKET) {
+			printf("Accept failed\n");
+			closesocket(s);
+			WSACleanup();
+			return 1;
+		}
+		char recvbuf[512];
+		int bytes_received = recv(client_socket, recvbuf, sizeof(recvbuf), 0);
+		if (bytes_received > 0) {
+			recvbuf[bytes_received] = '\0';
+			printf("Received: %s\n", recvbuf);
+		}
+		if (recvbuf == "map") {
 			string buf;
-			for(int i=0;i<=31;i++){
-				for(int j=0;j<=31;j++){
-					buf+=to_string(map[i][j]);
+			for (int i = 0; i <= 31; i++) {
+				for (int j = 0; j <= 31; j++) {
+					buf += to_string(map[i][j]);
 				}
 			}
 			char sendbuf[1024];
-			strcpy(sendbuf,buf.c_str());
-			send(client_socket,sendbuf,strlen(sendbuf),0);
-		}else{
-			int x=(recvbuf[0]-30)*10+(recvbuf[1]-30);
-			int y=(recvbuf[2]-30)*10+(recvbuf[3]-30);
-			map[x][y]=recvbuf[4]-30;
+			strcpy(sendbuf, buf.c_str());
+			send(client_socket, sendbuf, strlen(sendbuf), 0);
+		} else {
+			int x = (recvbuf[0] - 30) * 10 + (recvbuf[1] - 30);
+			int y = (recvbuf[2] - 30) * 10 + (recvbuf[3] - 30);
+			map[x][y] = recvbuf[4] - 30;
 		}
-		
+
 	}
 	closesocket(s);
 	closesocket(client_socket);
